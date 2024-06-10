b7a5b2ea6696d1da87cc6eac7be83207f4d854fe
rhuan <rhuan.gabriel@academico.ufpb.br>
2024-06-09 23:40:17 -0300

    Fase 3 (POO) - Criando novos menus no programa principal

--- Main.py
+++ Main.py
@@ -6,92 +6,98 @@ class Main:
     
     @classmethod
     def Menu(self):
-        print("""
-[1] Extrato
-[2] Saldo
-[3] Deposito
-[4] Saque
-[5] Abrir conta
-[6] Listar contas
-[7] Sair
-
-        """)
+        return("\nBem vindo! Selecione uma das opções abaixo:\n[1] Realizar operação\n[2] Abrir conta\n[3] Listar contas\n[4] Sair\nOperação desejada: ")
     
     @classmethod
-    def Inicio(self):
+    def MenuOperacao(self):
+        return("\nSelecione a operação desejada:\n1 - Extrato\n2 - Saldo\n3 - Deposito\n4 - Saque\nOperação desejada: ")
+    
+    @classmethod
+    def Cadastro(self):
         
         while(True):
-            
-            print("Bem vindo ao sistema bancário!\nSelecione uma opção:\n1 - Cadastrar PF\n2 - Cadastrar PJ\n3 - Sair\n")
-            
-            esc = int(input())
+
+            esc = int(input("\nBem vindo ao sistema bancário!\nSelecione uma opção:\n[1] - Cadastrar PF\n[2] - Cadastrar PJ\n[3] - Sair\nOpção desejada: "))
             
             if esc == 1:
-                print("Insira seus dados:")
+                print("Insira seus dados")
                 nome = input("Seu nome: ")
                 cpf = input("Seu CPF: ")
                 endereco = input("Seu endereço: ")
                 return Clientes.Pessoa_fisica(cpf, nome, endereco)
             
             elif esc == 2:
-                print("Insira dados da empresa:")
+                print("Insira dados da empresa")
                 nome = input("Nome da empresa: ")
                 cnpj = input("CNPJ da empresa: ")
                 endereco = input("Endereço da empresa: ")
                 return Clientes.Pessoa_Juridica(cnpj, nome, endereco)
+            
             elif esc == 3:
                 return None
+            
             else:
                 print("Escolha uma opção válida!\n")
 
     @classmethod
     def Main(self):
         
-        cliente = Main.Inicio()
+        cliente = Main.Cadastro()
         
         if cliente:
+            
             while(True):
-                Main.Menu()
-                escolha = int(input("Operação desejada: "))
                 
-                if escolha == 1: #Extrato
+                escolha = int(input(Main.Menu()))
+                
+                if escolha == 1: #Realizar operação
                     if cliente.contas:
-                        print("Selecione a conta que deseja conferir o extrato:")
-                        for i in range(0, len(cliente.contas)):
-                            print(f"Digite '{i}' para: {cliente.contas[i]}")
-                        conta_extrato = int(input())
-                        cliente.contas[conta_extrato].historico.exibir_extrato()
+                        operacao = int(input(Main.MenuOperacao()))
+                        
+                        if operacao == 1: #Extrato
+                            print("Selecione a conta que deseja conferir o extrato:")
+                            for i in range(0, len(cliente.contas)):
+                                print(f"Digite '{i}' para: {cliente.contas[i]}")
+                            conta_extrato = int(input("Conta selecionada: "))
+                            cliente.contas[conta_extrato].historico.exibir_extrato()
+                        
+                        if operacao == 2: #Saldo
+                            pass
+                        if operacao == 3: #Deposito
+                            pass
+                        if operacao == 4: #Saque
+                            pass
+                        else:
+                            print("Número de operação inválido! Tente novamente")
                     else:
-                        print("Não existem contas para exibir extrato")
-                elif escolha == 2: #Saldo
-                    pass
-                elif escolha == 3: #Deposito
-                    pass
-                elif escolha == 4: #Saque
-                    pass
-                elif escolha == 5: #Abrir conta
+                        print("Não existem contas para realizar operação")
+
+                elif escolha == 2: #Abrir conta
                     
                     tipo = int(input("Para conta corrente digite 1\nPara conta poupança digite 2\nEscolha: "))
                     
-                    if tipo == 1:
+                    if tipo == 1: #Conta corrente
                         
                         cliente.adicionar_conta(Contas.Conta_Corrente.nova_conta(cliente))
                         
-                    elif tipo == 2:
+                    elif tipo == 2: #Conta poupança
                         
                         cliente.adicionar_conta(Contas.Conta_Poupanca.nova_conta(cliente))
                     
                     else:
                         print("Opção inválida! Tente novamente.")
                         
-                elif escolha == 6: #Listar contas
+                elif escolha == 3: #Listar contas
                     pass
-                elif escolha == 7: #Sair
+                elif escolha == 4: #Sair
+                    print("Encerrando o sistema...")
                     break
                 else:
                     print("Escolha uma opção válida")
                     continue
-
+        else:
+            print("Encerrando o sistema...")
+            
 Main.Main()
 """ ------- testes--------
 
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ### Estagio 2 do sistema bancário em Python da DIO
 #### - Criando o sistima utilizando programação estruturada
+#### - O arquivo "BankMain.py" é totalmente voltado a implementação da fase 2
 
 ### Estagio 3 do sistema bancário em Python da DIO (Atual)
 #### - Reimplementação total do sistema utilizando POO
